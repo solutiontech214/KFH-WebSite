02f0a24ce3678ee67b0c3b578c28fbd6339bce90
samarth <samarthbagale9@gmail.com>
2024-01-15 22:23:49 +0530

    created login from

---
+++ Main/src/form.js
@@ -0,0 +1,21 @@
+
+let loader = document.getElementById("preLoader")
+
+window.addEventListener("load",()=>{
+ loader.style.display = "none";
+});
+
+function togglePasswordVisibility() {
+  const passwordInput = document.getElementById("passwordInput");
+  const eyeIcon = document.querySelector(".eye");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  }
+}
