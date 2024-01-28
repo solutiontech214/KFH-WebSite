e48b71a3d8d736648722049b0fb4cee625e3f96b
samarth <samarthbagale9@gmail.com>
2024-01-28 10:37:26 +0530

    Revert "made changes also added error msg space"
    This reverts commit 85fd9b4d5ac187644941d7dcedf6d798426e8301.

--- Main/src/js/index.js
+++ Main/src/js/index.js
@@ -2,35 +2,21 @@ var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);
+showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length };
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
+var i;
+var x = document.getElementsByClassName("mySlides");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length} ;
+for (i = 0; i < x.length; i++) {
+x[i].style.display = "none";
+}
+x[slideIndex-1].style.display = "block";
 }
 
 let loader = document.getElementById("preLoader");
-window.addEventListener("load", () => {
-    loader.style.display = "none";
-});
-
-
-const allImages = document.querySelectorAll('.container1 img');
-
-allImages.forEach(image => {
-    image.addEventListener('click', () => {
-        allImages.forEach(otherImage => {
-            if (otherImage !== image) {
-                otherImage.style.width = '10%';
-            }
-        });
-        image.style.width = '59.3%';
-    });
+window.addEventListener("load",()=>{
+loader.style.display = "none";
 });
