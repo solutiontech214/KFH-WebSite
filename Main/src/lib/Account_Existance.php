0307205684cad801fae69660d5a7ba824a37a937
onkar gutti <onkargutti94@gmail.com>
2024-01-30 21:26:58 +0530

    lib

--- Main/src/lib/Account_Existance.php
+++ Main/src/lib/Account_Existance.php
@@ -1,9 +1,22 @@
 <?php
 class Account_Existance
 {
+
+public function __construct()
+{
+    
+    if(mysqli_connect("localhost","KFH_DB","this.pass.123","KFH_DB"))
+    {
+        echo "connection established";
+    }
+}
+
     public function is_account_exists($email)
     {
 
     }
 }
+
+$a=new Account_Existance();
+
 ?>
