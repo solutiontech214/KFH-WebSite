9e37d75a8e3c9606332a253b87221c82ea7fb76c
onkar gutti <onkargutti94@gmail.com>
2024-02-23 20:49:03 +0530

    some new changes

--- Main/src/html/index.php
+++ Main/src/html/index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if(isset($_POST['log_out']))
+      {
+      unset($_SESSION['log']);
+      }
 ?><!DOCTYPE html>
 <html lang="en">
 
@@ -45,16 +49,25 @@ session_start();
 
     
     echo '<div class="login">
-      <a href="../html/login.php"><button class="btn-login" id="login" type="button" >Login</button></a>
+      <a href="../html/login.php"><button class="btn-login" id="login" type="button" >Login<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-door-open" viewBox="0 0 16 16">
+      <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1"/>
+      <path d="M10.828.122A.5.5 0 0 1 11 .5V1h.5A1.5 1.5 0 0 1 13 2.5V15h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V1.5a.5.5 0 0 1 .43-.495l7-1a.5.5 0 0 1 .398.117M11.5 2H11v13h1V2.5a.5.5 0 0 0-.5-.5M4 1.934V15h6V1.077z"/>
+    </svg></button></a>
     </div>';
     
     }
     else{
+      
       echo '<div class="login">
-      <a href="../html/index.php"><button class="btn-login" id="login" type="button" >Log Out</button></a>
+      <form method="post" >
+      <button class="btn-login" id="login" type="submit" name="log_out" value="Log Out">Log Out<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+      <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+    </svg></button>
+      </form
     </div>';
-    unset($_SESSION['log']);
-    }
+   
+  }
     ?>
   </nav>
 
---
+++ Main/src/html/redirect.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirecting..</title>
+</head>
+<body>
+   <h1> For Sending Feedback Please Login First..</h1>
+<h2>Redirecting to Login Page..</h2>
+</body>
+</html>
+<script>
+        // Delay the redirection for 3 seconds (3000 milliseconds)
+        setTimeout(function() {
+            window.location.href = 'login.php';
+        }, 3000);
+</script>
