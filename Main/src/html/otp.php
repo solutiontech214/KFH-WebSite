347674279078a38119b4dd93f2fd0f3e03913739
samarth <samarthbagale9@gmail.com>
2024-02-24 13:48:12 +0530

    ..

--- Main/src/html/otp.php
+++ Main/src/html/otp.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>OTP</title>
     <!-- Bootstrap CSS -->
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500&display=swap" rel="stylesheet">
