0af07e2711a7382a466153a2a938a29639b2f037
samarth <samarthbagale9@gmail.com>
2024-02-26 20:27:31 +0530

    aboutus start

--- Main/src/html/index.php
+++ Main/src/html/index.php
@@ -11,18 +11,14 @@ if(isset($_POST['log_out']))
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>KFH</title>
- 
 </head>
   <link rel="stylesheet" href="../css/indexStyle.css">
   <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;500&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <body>
-
   <div class="preloader" id="preLoader"></div>
-
   <nav class="nav-bar">
     <div style="margin-left: 10px;">
       <img src="../../Images/KFH.png" width="120px" height="50px" class="logo">
